baa8fe984be2a68dc9e097f59b57898bc5098dab
wekers <fernando@wekers.org>
2023-07-21 18:02:55 -0300

    Revert "Entidades"
    This reverts commit c3039ab2ba042e3bef88753333c1e89d15b4b1e2.

--- src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -1,7 +1,14 @@
 package com.devsuperior.dsmeta.entities;
 
-import javax.persistence.*;
 import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_sales")
