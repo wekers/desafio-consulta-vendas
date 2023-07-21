c3039ab2ba042e3bef88753333c1e89d15b4b1e2
wekers <fernando@wekers.org>
2023-07-21 18:00:53 -0300

    Entidades

--- src/main/java/com/devsuperior/dsmeta/entities/Seller.java
+++ src/main/java/com/devsuperior/dsmeta/entities/Seller.java
@@ -1,14 +1,8 @@
 package com.devsuperior.dsmeta.entities;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_seller")
