2ddd48a9e6d7908a22f22a98fc5e424af28a6daa
wekers <fernando@wekers.org>
2023-07-21 18:01:19 -0300

    Entidades

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,33 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SummaryProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    // SQL Nativo
+    @Query(nativeQuery = true,
+            value = "SELECT tb_seller.name, SUM(tb_sales.amount) AS salesAmount "
+                    + "FROM tb_sales "
+                    + "INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id "
+                    + "WHERE tb_sales.date BETWEEN :minDate AND :maxDate "
+                    + "GROUP BY tb_seller.name")
+    List<SummaryProjection> findSummaryNativeSQL(LocalDate minDate, LocalDate maxDate);
+
+
+
+    // JPQL
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
+            + "FROM Sale obj "
+            + "WHERE obj.date >= :minDate "
+            + "AND obj.date <= :maxDate "
+            + "GROUP BY obj.seller.name")
+    List<SaleSummaryDTO> findSummaryJPQL(LocalDate minDate, LocalDate maxDate);
 
 }
