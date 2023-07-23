18f313fc5e11d4a19ff72dece5d0dad28936472f
wekers <fernando@wekers.org>
2023-07-23 19:37:04 -0300

    Repository com JPQL e SQL Nativo

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -2,7 +2,10 @@ package com.devsuperior.dsmeta.repositories;
 
 import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.ReportProjection;
 import com.devsuperior.dsmeta.projections.SummaryProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,6 +13,39 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    /* ------------------- Report ------------------------------------------- */
+
+    // SQL Nativo - Paginado
+    @Query(nativeQuery = true,
+            value = "SELECT tb_sales.id, tb_sales.date, tb_sales.amount, tb_seller.name "
+                    + "FROM tb_sales "
+                    + "INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id "
+                    + "WHERE UPPER(tb_seller.name) LIKE CONCAT('%',UPPER(:name),'%') AND "
+                    + "tb_sales.date BETWEEN :minDate AND :maxDate",
+                    countQuery = "SELECT COUNT(*) FROM tb_sales INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id "
+                            + "WHERE UPPER(tb_seller.name) LIKE CONCAT('%',UPPER(:name),'%') AND "
+                            + "tb_sales.date BETWEEN :minDate AND :maxDate")
+    Page<ReportProjection> findReportNativeSQL(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
+
+    // JPQL - Paginado
+    @Query(value = "SELECT obj "
+                    + "FROM Sale obj "
+                    + "JOIN FETCH obj.seller "
+                    + "WHERE obj.date BETWEEN :minDate AND :maxDate "
+                    + "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name,'%'))",
+                    countQuery = "SELECT COUNT(obj) FROM Sale obj JOIN obj.seller "
+                            + "WHERE obj.date BETWEEN :minDate AND :maxDate "
+                            + "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name,'%'))")
+    Page<Sale> findReportJPQL(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
+
+    /* ------------------- End Report Query ---------------------------------- */
+
+
+
+
+
+    /* ------------------- Summary ------------------------------------------- */
 
     // SQL Nativo
     @Query(nativeQuery = true,
@@ -21,7 +57,6 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
     List<SummaryProjection> findSummaryNativeSQL(LocalDate minDate, LocalDate maxDate);
 
 
-
     // JPQL
     @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
             + "FROM Sale obj "
@@ -29,5 +64,7 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
             + "AND obj.date <= :maxDate "
             + "GROUP BY obj.seller.name")
     List<SaleSummaryDTO> findSummaryJPQL(LocalDate minDate, LocalDate maxDate);
+
+    /* ------------------- End Summary Query ---------------------------------- */
 
 }
